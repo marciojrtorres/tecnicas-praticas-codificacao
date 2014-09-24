e26cceae2f7b272d1d2bd201e03dbde31c47fb4b
Marcio Ramos <marciojrtorres@gmail.com>
2014-09-23 21:37:19 -0300

    exemplo clausula salvaguarda

--- exemplos/clausula-salvaguarda/src/Main.java
+++ exemplos/clausula-salvaguarda/src/Main.java
@@ -2,6 +2,13 @@ public class Main {
 	
     public static void main(String[] args) {
     	
+    	System.out.println(StringUtil.padleft("teste", 10, '-'));
+    	
+    	System.out.println(StringUtil.padleft("teste", 4, '-'));
+    	
+    	System.out.println(StringUtil.padright("teste", 10, '-'));
+    	
+    	System.out.println(StringUtil.padcenter("teste", 10, '-'));
 
     }
 }
--- exemplos/clausula-salvaguarda/src/StringUtil.java
+++ exemplos/clausula-salvaguarda/src/StringUtil.java
@@ -1,51 +1,8 @@
 public class StringUtil {
 
-    public static boolean eq(String s1, String s2) {
-        char[] chars1 = s1.toCharArray();
-        char[] chars2 = s2.toCharArray();
-
-        if (chars1.length != chars2.length) return false;
-
-        for (int i = 0; i < chars1.length; i++) if (chars1[i] != chars2[i]) return false;
-
-        return true;
-    }
-
-    public static String pad_velho(String s, int largura,
-                             char pad, boolean esquerda) {
-
-        char[] chars = s.toCharArray();
-
-        int faltam = largura - chars.length; // 10 - daniel.length == 4
-
-        char[] stringComPad = new char[largura]; // ex.: 10
-
-        for (int i = 0; i < largura; i++) {
-            if (esquerda) {
-                if (i < faltam) stringComPad[i] = pad;
-                else stringComPad[i] = chars[i - 4];
-            } else {
-                if (i < chars.length) stringComPad[i] = chars[i];
-                else stringComPad[i] = pad;
-            }
-        }
-
-        // [-,-,-,-,_,_,_,_,_,_]
-        // ----
-        //     daniel
-        // for (int i = i; i < chars.length; i++)
-        //    stringComPad[i + 4] = chars[i];
-
-        // [d,a,n,i,e,l]
-        // [-,-,-,-,_,_,_,_,_,_]
-        //          ^
-        // i = 4
-        //
-
-        return new String(stringComPad);
-    }
 
     public static String padleft(String s, int largura, char pad) {
+    	
         char[] chars = s.toCharArray();
 
         int faltam = largura - chars.length;
@@ -61,9 +18,8 @@ public class StringUtil {
     }
 
     public static String padright(String s, int largura, char pad) {
+    	
         char[] chars = s.toCharArray();
-
-        int faltam = largura - chars.length;
 
         char[] stringComPad = new char[largura];
 
@@ -79,13 +35,14 @@ public class StringUtil {
 
         char[] chars = s.toCharArray();
 
-        int faltam = largura - chars.length;
+        int pads = largura - chars.length; // 10 - 5 = 5
 
         char[] stringComPad = new char[largura];
 
         for (int i = 0; i < largura; i++) {
-            if (i < faltam) stringComPad[i] = pad;
-            else stringComPad[i] = chars[i - faltam];
+            if (i < pads / 2) stringComPad[i] = pad;
+            else if (i >= pads / 2 + chars.length) stringComPad[i] = pad;
+            else stringComPad[i] = chars[i - pads / 2]; 
         }
 
         return new String(stringComPad);
